bec28da10e430b77ae1bd48e8dade14032230af2
missipsag <ghernaoutmissipsa.pro@gmail.com>
2025-05-01 12:03:08 +0100

    added mysql2 client and dotenv

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const express = require("express");
-const colors =  require("colors")
+const colors = require("colors");
 const PORT = 3000;
+const mysql = require("mysql2");
 const App = express();
 
+if (process.env.NODE_ENV !== "production") {
+    require("dotenv").config();
+}
 
-
-
-App.listen(PORT , () => {
-    console.log(`\nSERVER RUNNING ON PORT ${PORT}`.blue);
+let connectDb = mysql.createConnection({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER, 
+    password: process.env.MYSQL_PASSWORD,
+    database: 'leCactus'
 })
+
+
+connectDb.connect(function (err) {
+    if (err) {
+        console.log("MySQL CONNECTION ERROR : ".red, err);
+    }
+    else console.log("MySQL DATABASE CONNECTED".blue);        
+});

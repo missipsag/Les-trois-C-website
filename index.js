4207bca94fd778804db3c35a86bdbaebfe2dbc62
missipsag <ghernaoutmissipsa.pro@gmail.com>
2025-05-16 11:08:54 +0100

    addes express session

--- index.js
+++ index.js
@@ -4,10 +4,26 @@ const PORT = 3000;
 const mysql = require("mysql2");
 const connectDB = require('./config/db.js');
 const App = express();
+const session = require("express-session");
 
 if (process.env.NODE_ENV !== "production") {
     require("dotenv").config();
 }
+
+// session config 
+const SESSION_CONFIG = {
+    name: 'session',
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.SESSION_SECRET,
+    cookie: {
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+        httpOnly:true
+    }
+}
+
+App.use(session(SESSION_CONFIG));
 
 // #Todos: 
 /* 

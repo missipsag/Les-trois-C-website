446cb5fc15e9d5183b8b7b5b29c1c8cd203ed089
missipsag <ghernaoutmissipsa.pro@gmail.com>
2025-05-17 11:22:24 +0100

    moved models to separate files

--- config/db.js
+++ config/db.js
@@ -1,43 +1,10 @@
 const { Module } = require("module");
 const mysql = require("mysql2"); 
 const { promisify } = require("util");
-if (process.env.NODE_ENV !== "production") {
-    require("dotenv").config();
-}
-
-const SQL_CREATE_USER_TABLE = "   CREATE TABLE  IF NOT EXISTS users ("
-    + "userId varchar(255) UNIQUE PRIMARY KEY NOT NULL,"
-    + "firstName varchar(255) NOT NULL ,"
-    + "lastName varchar(255) NOT NULL,"
-    + "email varchar(255) NOT NULL,"
-    + "phone varchar(255),"
-    + "role ENUM ('user', 'admin')"
-    + ");";
-
-const SQL_CREATE_ROOMS_TABLE = "CREATE TABLE IF NOT EXISTS rooms ("
-    + "roomId varchar(255) NOT NULL PRIMARY KEY,"
-    + "roomName varchar(255) ,"
-    + "capacity INTEGER NOT NULL, "
-    + "type ENUM ('conference', 'wedding', 'special occasion', 'other'),"
-    + "CHECK (capacity > 0) "
-    + " );";
-    
-const SQL_CREATE_RESERVATION_TABLE = "CREATE TABLE IF NOT EXISTS reservations ("
-    + "reservationId varchar(255) NOT NULL PRIMARY KEY,"
-    + "roomId varchar(255) NOT NULL,"
-    + "userId varchar(255) NOT NULL,"
-    + "date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,"
-    + "FOREIGN KEY (roomId) REFERENCES rooms(roomId),"
-    + "FOREIGN KEY (userId) REFERENCES users(userId)"
-    + ");" 
-
-const SQL_CREATE_REVIEW_TABLE = "CREATE TABLE IF NOT EXISTS reviews ("
-    + "reviewId varchar(255) NOT NULL PRIMARY KEY,"
-    + "roomId varchar(255) NOT NULL,"
-    + "userId varchar(255) NOT NULL,"
-    + "FOREIGN KEY (roomId) REFERENCES rooms(roomId),"
-    + "FOREIGN KEY (userId) REFERENCES users(userId)"
-    + ");";
+const SQL_CREATE_USER_TABLE = require("../models/UserModels");
+const SQL_CREATE_RESERVATION_TABLE = require("../models/ReservationModel");
+const SQL_CREATE_REVIEW_TABLE = require("../models/ReviewModel");
+const SQL_CREATE_ROOMS_TABLE = require("../models/RoomModel");
 
 const connectDB = mysql.createConnection({
     host: process.env.MYSQL_HOST,

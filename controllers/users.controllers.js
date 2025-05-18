fe8ba67596d6274795545eec3a0f821a01a5f49f
missipsag <ghernaoutmissipsa.pro@gmail.com>
2025-05-18 22:06:25 +0100

    added some basic controllers

--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -5,11 +5,29 @@ const {getUsers} = require("../models/UserModels")
 // ! ADD CRUD FOR USERS 
 //! ADD TRIGGERS FOR USERS
 
-getUsersData = async (req, res) => {
-    try {
-        let data = await getUsers();
-        return data;
-    } catch (err) {
-        res.json({msg : err, data})
-    }
+exports.getUsers = async () => {
+    let query = "select * from users";
+    const data = await promiseConnection(query);
+    return data;
+} 
+
+exports.getUserById = async (Id) => {
+    const query = ` SELECT * FROM users ` 
+        + `WHERE userdId = ${Id};`
+    
+    const foundUser = await promiseConnection(query);
+    return foundUser;
+}
+
+
+exports.updateUserById = async (Id, firstName, lastName, email, phone) => {
+    const query = ` UPDATE TABLE users SET`
+        + `firstName = ${firstName}`
+        + `lastName = ${lastName}`
+        + `email = ${email}`
+        + `phone = ${phone}`
+        + `WHERE userId = ${Id};`
+    
+    const updatedUser = await promiseConnection(query);
+    return updatedUser;
 }

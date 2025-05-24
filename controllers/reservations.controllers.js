557f6398f69c646b281579e2047e6fd7719e9e93
missipsag <ghernaoutmissipsa.pro@gmail.com>
2025-05-24 15:21:01 +0100

    added create reservation

--- controllers/reservations.controllers.js
+++ controllers/reservations.controllers.js
@@ -1,3 +1,16 @@
-const promiseConnection = require("../config/db.js");
+const {promiseConnection} = require("../config/db.js");
 
 
+module.exports.createReservation = async function (
+    reservationId,
+    roomId, 
+    userId, 
+    reservationDate
+) {
+    const sql = "INSERT INTO reservations (reservationId, roomId, userId, reservationDate)"
+                + `VALUES ('${reservationId}', '${roomId}', '${userId}', '${reservationDate}');`
+    
+    const createdReservation = await promiseConnection(sql);
+
+    return createdReservation;
+}

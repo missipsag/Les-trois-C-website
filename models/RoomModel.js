29fb69af6a4e411459e53e69fa063a3c9895a18f
missipsag <ghernaoutmissipsa.pro@gmail.com>
2025-05-24 15:24:08 +0100

    modified type attribute, got rid of 'other'

--- models/RoomModel.js
+++ models/RoomModel.js
@@ -2,7 +2,7 @@ module.exports.SQL_CREATE_ROOMS_TABLE = "CREATE TABLE IF NOT EXISTS rooms ("
     + "roomId varchar(255) NOT NULL PRIMARY KEY,"
     + "roomName varchar(255) ,"
     + "capacity INTEGER NOT NULL, "
-    + "type ENUM ('conference', 'wedding', 'special occasion', 'other'),"
+    + "type ENUM ('conference', 'wedding', 'special occasion'),"
     + "CHECK (capacity > 0) "
     + " );";
     
